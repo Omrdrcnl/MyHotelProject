bfd9f09da746bca94c604336138d315e08de0053
Ömer DİRİCANLI <omrdrcnl@gmail.com>
2023-10-18 17:35:36 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.DataAccessLayer/concrete/Context.cs
@@ -0,0 +1,19 @@
+﻿using HotelProject.EntityLayer.concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelProject.DataAccessLayer.concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=Omer-Diricanli\\SQLEXPRESS; initial catalog=ApiDb; integrated security=true; TrustServerCertificate=True ");
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staff { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
